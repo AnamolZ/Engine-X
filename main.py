a364b6251be6ef4f7f02a8254190c4e9364d0fdc
Anamol <anmoldkl971@gmail.com>
2023-11-18 17:17:58 +0545

    version 1.0

--- main.py
+++ main.py
@@ -19,3 +19,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+logging.basicConfig(filename='error.log', level=logging.ERROR)
+
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+def get_random_animal_photo(api_key: str = 'pOoUofTGrf9bk53QeVlJF4jUt0akLDdoEgm6wMOt2Cw'):
+    base_url = "https://api.unsplash.com/photos/random"
+    params = {"query": "random", "client_id": api_key}
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    if response.status_code == 200 and data:
+        return data["urls"]["regular"]
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request, api_key: str = Depends(get_random_animal_photo)):
+    photo_url = api_key
+    return templates.TemplateResponse("index.html", {"request": request, "photo_url": photo_url})
