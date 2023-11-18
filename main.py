42f4a308582cb55432603dfb2521c28d954b3404
Anamol <anmoldkl971@gmail.com>
2023-11-18 17:17:36 +0545

    version 1.0

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
